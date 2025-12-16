fa14342c05c7ebefd0d6dc5885a29f20473d7151
GuilhermeAnitelli <guilherme.anitelli10@gmail.com>
2025-12-16 12:01:32 -0300

    Adicionar funcionalidade de alternância de tema com JavaScript e atualizar estilos CSS para se adequar com o tema.

---
+++ assets/js/theme.js
@@ -0,0 +1,17 @@
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'light') {
+    body.classList.add('light-mode');
+}
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('light-mode');
+    
+    if (body.classList.contains('light-mode')) {
+        localStorage.setItem('theme', 'light');
+    } else {
+        localStorage.setItem('theme', 'dark');
+    }
+});
